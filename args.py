51994860f355b61357209981fe8504c44fe69094
zhangzheng0908 <112859328+zhangzheng0908@users.noreply.github.com>
2023-09-05 17:55:39 +0800

    Add files via upload

---
+++ args.py
@@ -0,0 +1,21 @@
+### CONFIGS ###
+dataset = 'cora'
+model = 'GAE'
+
+###input_dim = 1433 
+###hidden1_dim = 32
+###hidden2_dim = 16
+###use_feature = True
+
+###num_epoch = 200
+###learning_rate = 0.01
+
+
+
+input_dim = 1433
+hidden1_dim = 32*2*32
+hidden2_dim = 16*2*32
+use_feature = True
+
+num_epoch = 150
+learning_rate = 0.01125
